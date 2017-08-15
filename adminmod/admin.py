7205384a698ac0dc458f6352393ec00db31d6a5b
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-15 20:42:39 -0300

    admin created

--- adminmod/admin.py
+++ adminmod/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SalesSummary
 
-# Register your models here.
+
+@admin.register(SalesSummary)
+class SalesSummaryAdmin(ModelsAdmin):
+    change_list_template = 'admin/sale_summary_change_list.html'
+    date_hierarchy = 'created'
