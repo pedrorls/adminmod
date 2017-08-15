744bfac6a7dc1d7ff688cc6aaf688eeb0e4f0b30
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-15 20:24:31 -0300

    model sales summary created

--- adminmod/models.py
+++ adminmod/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class SalesSummary(Sales):
+    class Meta:
+        proxy = True
+        verbose_name = 'Sale Summary'
+        verbose_name_plural = 'Sales Summary'
