3b2256f507b31ed17fa12343c8d2f632e4c9934c
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-15 21:15:36 -0300

    new template created

--- adminmod/admin.py
+++ adminmod/admin.py
@@ -3,6 +3,6 @@ from .models import SalesSummary
 
 
 @admin.register(SalesSummary)
-class SalesSummaryAdmin(ModelsAdmin):
+class SalesSummaryAdmin(admin.ModelAdmin):
     change_list_template = 'admin/sale_summary_change_list.html'
     date_hierarchy = 'created'
